f07f1e0bf4663eeadf5d9c05b28a516172438741
Ilkut Kutlar <ilkutkutlar@gmail.com>
2020-08-31 17:27:56 +0300

    implement hints_for_field method

--- minesweeper_model/field.py
+++ minesweeper_model/field.py
@@ -13,41 +13,43 @@ class Field:
     def open_tile(x, y):
         pass
 
-def generate_hints(width, height, mines):
-    pass
+class HintGenerator:
+    @staticmethod
+    def hints_for_field(field_width, field_height, mines):
+        return {(x, y): HintGenerator.hint_for_tile(x, y, field_width, field_height, mines)
+                for x in range(field_width) 
+                for y in range(field_height)}
 
-def hint_for_tile(tile_x, tile_y, field_width, field_height, mines):
-    """Return the hint for the given tile.
+    @staticmethod
+    def hint_for_tile(tile_x, tile_y, field_width, field_height, mines):
+        """Return the hint for the given tile.
 
-    Parameters:
-        tile_x: X coord of tile
-        tile_y: Y coord of tile
-        field_with: width of Minesweeper field
-        field_height: height of Minesweeper field
-        mines (list of two-tuples): list of mine coords
-    
-    Returns:
-        Hint for the given tile.
-    """
+        Parameters:
+            tile_x: X coord of tile
+            tile_y: Y coord of tile
+            field_with: width of Minesweeper field
+            field_height: height of Minesweeper field
+            mines (list of two-tuples): list of mine coords
+        
+        Returns:
+            Number of mines in surrounding tiles (hint) 
+            for the given tile, or -1 if tile being checked has a mine.
+        """
 
-    def is_mine(x, y):
-        # is tile outside the bounds of the Minesweeper field?
-        is_outside_bottom_left = (x < 0) or (y < 0)
-        is_outside_top_right = (x > field_width - 1) or (y > field_height - 1)
+        if (tile_x, tile_y) in mines:
+            return -1
+        
+        # Loops generate all surrounding tiles and the tile itself
+        surrounding_tiles = [(x, y) for x in [tile_x - 1, tile_x, tile_x + 1] 
+                                    for y in [tile_y - 1, tile_y, tile_y + 1]]
 
-        if is_outside_bottom_left:
-            return False
-        elif is_outside_top_right:
-            return False
-        else:
-            return (x, y) in mines
-    
-    surrounding_tiles = [
-        (tile_x - 1, tile_y + 1), (tile_x, tile_y + 1), (tile_x + 1, tile_y + 1),
-        (tile_x - 1, tile_y), (tile_x + 1, tile_y),
-        (tile_x - 1, tile_y - 1), (tile_x, tile_y - 1), (tile_x + 1, tile_y - 1)
-    ]
+        # Remove the tile we are checking to only leave surrounding tiles
+        surrounding_tiles.remove((tile_x, tile_y))
 
-    surrounding_tile_mines = [is_mine(x, y) for x, y in surrounding_tiles]
-    return surrounding_tile_mines.count(True)
+        # No need to worry about edge cases caused by -ve tile coords in 
+        # surrounding_tiles as the mines list simply won't have the -ve 
+        # coords and those coords will register as False, as desired
+        surrounding_tiles_have_mine = [(x, y) in mines for (x, y) in surrounding_tiles]
+
+        return surrounding_tiles_have_mine.count(True)
 
--- tests/test_field.py
+++ tests/test_field.py
@@ -2,21 +2,38 @@ import unittest
 from minesweeper_model.field import *
 
 class TestField(unittest.TestCase):
+    def setUp(self):
+        self.width = 8
+        self.height = 5
+        self.mines = [(2, 0), (4, 2), (6, 4)]
 
-    def test_hint_for_tile(self):
-        width = 8
-        height = 5
-        mines = [(2, 0), (4, 2), (6, 4)]
-        
         # X: mines
-        # -: test positions
         #
         # ......X.
-        # .....-..
-        # ...-X...
         # ........
-        # -.X.....
+        # ....X...
+        # ........
+        # ..X.....
 
-        self.assertEqual(hint_for_tile(0, 0, width, height, mines), 0)
-        self.assertEqual(hint_for_tile(3, 2, width, height, mines), 1)
-        self.assertEqual(hint_for_tile(5, 3, width, height, mines), 2)
+    def test_hints_for_field(self):
+        actual = HintGenerator.hints_for_field(self.width, self.height, self.mines)
+        expected = {
+                    (0, 4): 0, (1, 4): 0, (2, 4): 0, (3, 4): 0, (4, 4): 0, (5, 4): 1, (6, 4): -1, (7, 4): 1,
+                    (0, 3): 0, (1, 3): 0, (2, 3): 0, (3, 3): 1, (4, 3): 1, (5, 3): 2, (6, 3): 1, (7, 3): 1,
+                    (0, 2): 0, (1, 2): 0, (2, 2): 0, (3, 2): 1, (4, 2): -1, (5, 2): 1, (6, 2): 0, (7, 2): 0,
+                    (0, 1): 0, (1, 1): 1, (2, 1): 1, (3, 1): 2, (4, 1): 1, (5, 1): 1, (6, 1): 0, (7, 1): 0,
+                    (0, 0): 0, (1, 0): 1, (2, 0): -1, (3, 0): 1, (4, 0): 0, (5, 0): 0, (6, 0): 0, (7, 0): 0
+                }
+        self.assertEqual(actual, expected)
+
+
+    def test_hint_for_tile(self):
+        check_mines = [(0, 0), (3, 2), (5, 3), (4, 2)]
+        expected_hints = [0, 1, 2, -1]
+
+        for mine, expected in zip(check_mines, expected_hints):
+            actual = HintGenerator.hint_for_tile(mine[0], mine[1],
+                                                 self.width, self.height,
+                                                 self.mines)
+            self.assertEqual(actual, expected)
+
