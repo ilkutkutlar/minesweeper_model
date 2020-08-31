4bf0c85a363ac57f7e0ac26a771adb05e6de2735
Ilkut Kutlar <ilkutkutlar@gmail.com>
2020-08-31 16:21:22 +0300

    implement hint_for_tile method

--- minesweeper_model/field.py
+++ minesweeper_model/field.py
@@ -16,6 +16,38 @@ class Field:
 def generate_hints(width, height, mines):
     pass
 
-def hint_for_tile(x, y, mines):
-    return 1
+def hint_for_tile(tile_x, tile_y, field_width, field_height, mines):
+    """Return the hint for the given tile.
+
+    Parameters:
+        tile_x: X coord of tile
+        tile_y: Y coord of tile
+        field_with: width of Minesweeper field
+        field_height: height of Minesweeper field
+        mines (list of two-tuples): list of mine coords
+    
+    Returns:
+        Hint for the given tile.
+    """
+
+    def is_mine(x, y):
+        # is tile outside the bounds of the Minesweeper field?
+        is_outside_bottom_left = (x < 0) or (y < 0)
+        is_outside_top_right = (x > field_width - 1) or (y > field_height - 1)
+
+        if is_outside_bottom_left:
+            return False
+        elif is_outside_top_right:
+            return False
+        else:
+            return (x, y) in mines
+    
+    surrounding_tiles = [
+        (tile_x - 1, tile_y + 1), (tile_x, tile_y + 1), (tile_x + 1, tile_y + 1),
+        (tile_x - 1, tile_y), (tile_x + 1, tile_y),
+        (tile_x - 1, tile_y - 1), (tile_x, tile_y - 1), (tile_x + 1, tile_y - 1)
+    ]
+
+    surrounding_tile_mines = [is_mine(x, y) for x, y in surrounding_tiles]
+    return surrounding_tile_mines.count(True)
 
--- tests/test_field.py
+++ tests/test_field.py
@@ -2,5 +2,21 @@ import unittest
 from minesweeper_model.field import *
 
 class TestField(unittest.TestCase):
-    def test_generate_hints(self):
-        self.assertEqual(hint_for_tile(0,0,0), 0)
+
+    def test_hint_for_tile(self):
+        width = 8
+        height = 5
+        mines = [(2, 0), (4, 2), (6, 4)]
+        
+        # X: mines
+        # -: test positions
+        #
+        # ......X.
+        # .....-..
+        # ...-X...
+        # ........
+        # -.X.....
+
+        self.assertEqual(hint_for_tile(0, 0, width, height, mines), 0)
+        self.assertEqual(hint_for_tile(3, 2, width, height, mines), 1)
+        self.assertEqual(hint_for_tile(5, 3, width, height, mines), 2)
