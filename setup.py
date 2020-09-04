1eefa2033947d834d22f663ff7862f771bc55127
Ilkut Kutlar <ilkutkutlar@gmail.com>
2020-09-05 01:48:46 +0300

    add setup.cfg file

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@ from distutils.core import setup
 
 setup(name='minesweeper_model',
       version='0.0.1',
-      description='Minesweeper Model',
+      description='Tools to model a Minesweeper game and interact with it programmatically.',
       author='Ilkut Kutlar',
       author_email='ilkutkutlar@gmail.com',
       url='https://github.com/ilkutkutlar/minesweeper-model',
+      keywords=['minesweeper', 'model'],
       license='MIT',
-      packages=['minesweeper_model']
+      packages=['minesweeper_model'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6'
+      ],
       )
