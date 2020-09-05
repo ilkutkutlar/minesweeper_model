8d2f412077528064c9ce42e02e95021c37f4c9f4
Ilkut Kutlar <ilkutkutlar@gmail.com>
2020-09-05 03:27:30 +0300

    increase version to 0.0.2

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(name='minesweeper_model',
-      version='0.0.1',
+      version='0.0.2',
       description='Tools to model a Minesweeper game and interact with it programmatically.',
       long_description=long_description,
       long_description_content_type='text/markdown',
       author='Ilkut Kutlar',
       author_email='ilkutkutlar@gmail.com',
       url='https://github.com/ilkutkutlar/minesweeper_model',
-      download_url='https://github.com/ilkutkutlar/minesweeper_model/archive/v0.0.1.tar.gz',
+      download_url='https://github.com/ilkutkutlar/minesweeper_model/archive/v0.0.2.tar.gz',
       keywords=['minesweeper', 'model'],
       license='MIT',
       packages=['minesweeper_model'],
