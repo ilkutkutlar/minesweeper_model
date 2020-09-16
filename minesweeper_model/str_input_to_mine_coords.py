b243712702d0fe2d8020f475d5d9c3d0277b8b5e
Ilkut Kutlar <ilkutkutlar@gmail.com>
2020-09-16 21:19:47 +0300

    refactory str_input_to_mine_coords

--- minesweeper_model/str_input_to_mine_coords.py
+++ minesweeper_model/str_input_to_mine_coords.py
@@ -1,22 +1,18 @@
 def str_input_to_mine_coords(input_string):
-	mine_coord=[]
-	x_len,y_len=0,0
+    mine_coord = []
+    x_len, y_len = 0, 0
 
-	temp=input_string.split("\n")
-	x_len,y_len=len(temp[0]),len(temp) 
+    temp = input_string.split("\n")
+    x_len, y_len = len(temp[0]), len(temp)
 
-	x_mine,y_mine=0,0
+    x_mine, y_mine = 0, 0
 
-	for i in temp:
-		x_mine=0
-		for s in i:
-			if(s=='x'):
-				mine_coord.append((x_mine,y_mine))
-			x_mine+=1
-		y_mine+=1		
+    for i in temp:
+        x_mine = 0
+        for s in i:
+            if(s == 'x'):
+                mine_coord.append((x_mine, y_mine))
+            x_mine += 1
+        y_mine += 1
 
-	return (x_len, y_len, mine_coord)	
-
-
-
-	
+    return (x_len, y_len, mine_coord)
