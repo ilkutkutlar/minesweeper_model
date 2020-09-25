fc2e23b7bf6d5a9a56df5be95b2c994d312bdfdd
Ilkut Kutlar <ilkutkutlar@gmail.com>
2020-09-25 23:23:01 +0300

    added tests to check coordinates in string type accepted

--- tests/test_field.py
+++ tests/test_field.py
@@ -32,6 +32,11 @@ class TestField(unittest.TestCase):
         expected = {"hint": None, "flag": False}
         self.assertEqual(actual, expected)
 
+    def test_tile_string_coordinates(self):
+        actual = self.field2.tile("1", "0")
+        expected = {"hint": None, "flag": False}
+        self.assertEqual(actual, expected)
+
     def test_nine_tiles(self):
         # .: closed tile
         # [0-9]: open tile, hint
@@ -43,7 +48,6 @@ class TestField(unittest.TestCase):
         # x x . .
         # . . . .
 
-        actual = self.field2.nine_tiles(0, 1)
         # Expect to exclude tiles outside the field.
         expected = {(0, 0): {"hint": None, "flag": True},
                     (0, 2): {"hint": None, "flag": False},
@@ -52,6 +56,20 @@ class TestField(unittest.TestCase):
                     (1, 2): {"hint": None, "flag": False},
                     (0, 1): {"hint": 2, "flag": False}}
 
+        actual = self.field2.nine_tiles(0, 1)
+        self.assertEqual(actual, expected)
+
+    def test_nine_tiles_string_coordinates(self):
+        # Expect to exclude tiles outside the field.
+        expected = {(0, 0): {"hint": None, "flag": True},
+                    (0, 2): {"hint": None, "flag": False},
+                    (1, 0): {"hint": None, "flag": False},
+                    (1, 1): {"hint": 2, "flag": False},
+                    (1, 2): {"hint": None, "flag": False},
+                    (0, 1): {"hint": 2, "flag": False}}
+
+        # Coordinates are string, but still numeric
+        actual = self.field2.nine_tiles("0", "1")
         self.assertEqual(actual, expected)
 
     def test_open_tile(self):
@@ -65,6 +83,15 @@ class TestField(unittest.TestCase):
         self.assertFalse(self.field1.open_tile(0, 0))
         self.assertEqual(self.field1.open_coords, {(1, 0)})
 
+    def test_open_tile_string_coordinates(self):
+        # Coordinates are string, but still numeric
+        self.assertTrue(self.field1.open_tile("1", "0"))
+        self.assertEqual(self.field1.open_coords, {(1, 0)})
+
+    def test_open_tile_non_numeric_coordinates(self):
+        # "x" is a non-numeric string, so we should get an error
+        self.assertRaises(ValueError, self.field1.open_tile, "x", 0)
+
     def test_open_tile_open_adjacent_tiles(self):
         # field3
         # mines:        hints:      expected open:
@@ -77,8 +104,6 @@ class TestField(unittest.TestCase):
         # .x......      1x101110    .x+++...
         # .....x..      11101x10    ..+++x..
 
-        self.field3.open_tile(5, 1, True)
-
         # expect adjacent 0 hint tiles to open recursively until a non-zero hint tile
         # is found, when the adjacent tiles are opened the last time and algorithm stops.
         expected_open_coords = {(3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
@@ -88,6 +113,22 @@ class TestField(unittest.TestCase):
                                 (2, 4), (3, 4), (4, 4), (5, 4),
                                 (2, 5), (3, 5), (4, 5), (5, 5),
                                 (2, 6), (3, 6), (4, 6)}
+
+        self.field3.open_tile(5, 1, True)
+        self.assertEqual(self.field3.open_coords, expected_open_coords)
+
+    def test_open_tile_open_adjacent_tiles_string_coordinates(self):
+        # expect adjacent 0 hint tiles to open recursively until a non-zero hint tile
+        # is found, when the adjacent tiles are opened the last time and algorithm stops.
+        expected_open_coords = {(3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
+                                (3, 1), (4, 1), (5, 1), (6, 1), (7, 1),
+                                (3, 2), (4, 2), (5, 2), (6, 2), (7, 2),
+                                (2, 3), (3, 3), (4, 3), (5, 3),
+                                (2, 4), (3, 4), (4, 4), (5, 4),
+                                (2, 5), (3, 5), (4, 5), (5, 5),
+                                (2, 6), (3, 6), (4, 6)}
+
+        self.field3.open_tile("5", "1", True)
         self.assertEqual(self.field3.open_coords, expected_open_coords)
 
     def test_toggle_flag(self):
@@ -98,6 +139,11 @@ class TestField(unittest.TestCase):
         self.field1.toggle_flag(2, 0)
         self.assertEqual(self.field1.flag_coords, set())
 
+    def test_toggle_flag_string_coordinates(self):
+        self.field1.toggle_flag("0", "1")
+        self.assertEqual(self.field1.flag_coords, {(0, 1)})
+
+    def test_toggle_flag_invalid_coordinates(self):
         self.assertRaises(ValueError, self.field1.toggle_flag, 5, 5)
 
     def test_traverse_tiles(self):
@@ -114,8 +160,20 @@ class TestField(unittest.TestCase):
         def should_visit(x, y, field):
             return field.hints[(x, y)] == 0
 
-        actual = self.field3.traverse_tiles(0, 0, should_visit)
         expected = {(0, 0), (0, 1), (0, 2), (0, 3)}
+
+        actual = self.field3.traverse_tiles(0, 0, should_visit)
+        self.assertEqual(actual, expected)
+
+    def test_traverse_tiles_string_coordinates(self):
+        # Adjacent 0: [(4, 0), (4, 1), (4, 2), (5, 0), (6, 0), (6, 1)]
+
+        def should_visit(x, y, field):
+            return field.hints[(x, y)] == 0
+
+        expected = {(0, 0), (0, 1), (0, 2), (0, 3)}
+
+        actual = self.field3.traverse_tiles("0", "0", should_visit)
         self.assertEqual(actual, expected)
 
     def test_are_coords_valid(self):
